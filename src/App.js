17ef7d42d8495a47e3ae2ebc20d7fc4f2c2d8b40
souravtyson <souravtyson@gmail.com>
2023-06-04 23:40:36 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
-import "./style.css";
+import React, { useState, useMemo } from 'react';
+import './style.css';
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+
+  const addTask = (event) => {
+    event.preventDefault();
+    setTaskList([...taskList, task]);
+    setTask('');
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <form onSubmit={addTask}>
+        <label>Add Task</label>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {taskList.map((tas, index) => {
+        return (
+          <div key={`${tas}-${index}`}>
+            <input
+              type="checkbox"
+              value={tas}
+              name={tas}
+              id={`${tas}-${index}`}
+            />
+            <label htmlFor={`${tas}-${index}`}>{tas}</label>
+          </div>
+        );
+      })}
     </div>
   );
 }
